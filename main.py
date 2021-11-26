75d2cc849a0866372979d2a3d3e8e436aad319f6
Omar Terrazas <terrazas.omar@gmail.com>
2021-11-25 19:17:08 -0600

    First request

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello():
-    return 'Hello world from Flask'
+    user_ip = request.remote_addr
+
+    return 'Hello world from Flask, your ip is {}'.format(user_ip)
 
