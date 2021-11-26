b4b9cf537c9be1715b3e58b477b0e362b9839b67
Omar Terrazas <terrazas.omar@gmail.com>
2021-11-26 14:50:25 -0600

    Use session

--- main.py
+++ main.py
@@ -1,25 +1,27 @@
-from flask import Flask, request, make_response, redirect, render_template
+from flask import Flask, request, make_response, redirect, render_template, session
 from flask_bootstrap import Bootstrap
-
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 
+app.config['SECRET_KEY'] = 'secret'
+
 todos = ['Drink coffe','Exercise','Program Python','Sleep']
+
 
 @app.route('/')
 def index():
     user_ip = request.remote_addr
 
     response = make_response(redirect('/hello'))
-    response.set_cookie('user_ip', user_ip)
+    session['user_ip'] = user_ip
 
     return response
 
 
 @app.route('/hello')
 def hello():
-    user_ip = request.cookies.get('user_ip')
+    user_ip = session.get('user_ip')
     context = {'user_ip': user_ip, 'todos': todos}
 
     return render_template('hello.html', **context)
