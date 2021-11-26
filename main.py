cfe68a0fc1609e55841057cdf3bc686b90f24288
Omar Terrazas <terrazas.omar@gmail.com>
2021-11-25 19:21:18 -0600

    First response

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
-from flask import Flask, request
+from flask import Flask, request, make_response, redirect
+from werkzeug.wrappers import response
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
+def index():
     user_ip = request.remote_addr
+
+    response = make_response(redirect('/hello'))
+    response.set_cookie('user_ip', user_ip)
+
+    return response
+
+
+@app.route('/hello')
+def hello():
+    user_ip = request.cookies.get('user_ip')
 
     return 'Hello world from Flask, your ip is {}'.format(user_ip)
 
