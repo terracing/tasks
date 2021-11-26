aafb6898c45e36b7da493a12c96d7588d9bbc312
Omar Terrazas <terrazas.omar@gmail.com>
2021-11-26 13:52:15 -0600

    Load Bootstrap Framework

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from flask import Flask, request, make_response, redirect, render_template
+from flask_bootstrap import Bootstrap
+
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
 todos = ['Drink coffe','Exercise','Program Python','Sleep']
 
