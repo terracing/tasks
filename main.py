0cdfacdb96df63f794531aea78edf2c05b06d074
Omar Terrazas <terrazas.omar@gmail.com>
2021-11-26 18:40:29 -0600

    Using WTF Forms

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from flask import Flask, request, make_response, redirect, render_template, session
 from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms.fields import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
@@ -7,6 +10,11 @@ bootstrap = Bootstrap(app)
 app.config['SECRET_KEY'] = 'secret'
 
 todos = ['Drink coffe','Exercise','Program Python','Sleep']
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Submit')
 
 
 @app.route('/')
@@ -19,10 +27,20 @@ def index():
     return response
 
 
-@app.route('/hello')
+@app.route('/hello', methods=['GET', 'POST'])
 def hello():
     user_ip = session.get('user_ip')
-    context = {'user_ip': user_ip, 'todos': todos}
+    login_form = LoginForm()
+    username = session.get('username')
+
+    context = {'user_ip': user_ip, 'todos': todos, 'login_form': login_form, 'username': username}
+
+    if login_form.validate_on_submit():
+        username = login_form.username.data
+        session['username'] = username
+
+        response = make_response(redirect('/'))
+        return response
 
     return render_template('hello.html', **context)
 
