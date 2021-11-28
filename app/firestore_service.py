fe603f41b5ec94f3ec00a3bc090d2edca0a8bbe0
Omar Terrazas <terrazas.omar@gmail.com>
2021-11-27 22:54:15 -0600

    Create add tasks method

--- app/firestore_service.py
+++ app/firestore_service.py
@@ -24,3 +24,8 @@ def put_user(user_data):
     user_ref = db.collection('users').document(user_data.username)
     user_ref.set({'password': user_data.password})
 
+
+def put_todo(user_id, description):
+    todos_collection_ref = db.collection('users').document(user_id).collection('todos')
+    todos_collection_ref.add({'description': description})
+
--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import unittest
-from flask import request, make_response, redirect, render_template, session, flash
+from flask import request, make_response, redirect, render_template, session, flash, url_for
 from flask_login import login_required, current_user
 
 from app import create_app
-from app.forms import LoginForm
-from app.firestore_service import get_users, get_todos
+from app.forms import TodoForm
+from app.firestore_service import get_todos, put_todo
 
 app = create_app()
 
@@ -25,13 +25,21 @@ def index():
     return response
 
 
-@app.route('/hello')
+@app.route('/hello', methods=['GET', 'POST'])
 @login_required
 def hello():
     user_ip = session.get('user_ip')
     username = current_user.id
+    todo_form = TodoForm()
 
-    context = {'user_ip': user_ip, 'todos': get_todos(user_id=username), 'username': username}  
+    context = {'user_ip': user_ip, 'todos': get_todos(user_id=username), 'todo_form': todo_form, 'username': username}  
+
+    if todo_form.validate_on_submit():
+        put_todo(user_id = username, description = todo_form.description.data)
+
+        flash('Your task was created correctly')
+
+        return redirect(url_for('hello'))
 
     return render_template('hello.html', **context)
 
